129d4cc8ebe1cff633eef0e7c888a4d805ef8507
geekgrunge <dleuliette@gmail.com>
2013-02-27 17:30:57 +0100

    add clipboard support in progress

--- js/app.js
+++ js/app.js
@@ -13,10 +13,26 @@ $(document).ready(function(){
 			);
 		$("#e-hex").html($(this).attr("entity-hex"));
 		$("#e-class").html(
-			'<span>.</span>'+
+			'.'+
 			$(this).attr("class")
 			);
-
 	}); 
+
+	$(".clipboard").click(function(){
+		var texte = $(this).html();
+		var entity = new RegExp('<span>&amp;</span>');
+		//console.log(entity.test($(this).html()));
+		var retour = $('#e-name').val();
+
+		if (entity.test(texte) == true) {
+			retour = texte.replace("<span>&amp;</span>","")
+			retour = "&"+retour;
+		}
+		
+
+		console.log(texte);
+		window.prompt ("Copy to clipboard : Ctrl+C", String(retour));	
+	});
+
 
 });	
