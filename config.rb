e86f0b70bb7a1857fbf608c2f5ca40a0960ace15
David Leuliette <dleuliette@gmail.com>
2016-03-04 11:58:14 +0100

    Switch to gh-pages

--- config.rb
+++ config.rb
@@ -1,3 +1,12 @@
+require 'slim'
+
+set :js_dir,     'assets/javascripts'
+set :css_dir,    'assets/stylesheets'
+set :images_dir, 'assets/images'
+
+activate :autoprefixer, browsers: ['last 2 versions', 'ie 8', 'ie 9']
+activate :livereload
+
 activate :google_analytics do |ga|
   ga.tracking_id = data.settings.google_analytics.tracking_code
   ga.anonymize_ip = true
@@ -11,20 +20,12 @@ activate :livereload
 activate :autoprefixer,
   browsers: ['last 2 versions', 'ie 8', 'ie 9', 'ie 10']
 
-page "/sitemap.xml", :layout => false
-
-require 'slim'
-
-set :css_dir, 'assets/stylesheets'
-set :js_dir, 'assets/javascripts'
-set :images_dir, 'assets/images'
+page "/sitemap.xml", layout: false
 
 # Add bower's directory to sprockets asset path
 after_configuration do
-
   @bower_config = JSON.parse(IO.read("#{root}/.bowerrc"))
   sprockets.append_path File.join "#{root}", @bower_config["directory"]
-
 end
 
 configure :build do
@@ -35,4 +36,8 @@ configure :build do
   activate :favicon_maker
 
   activate :sitemap, :hostname => data.settings.site.url
+
+  # Use this for gh-pages
+  activate :relative_assets
+  set :relative_links, true
 end
